f8a3c263443727415c13f2ebdf84607c0e7dc1f6
Adrian <pata.adi@yahoo.com>
2022-11-28 21:18:17 +0200

    Initial commit

---
+++ src/test/java/dropDowns/oldStyle.java
@@ -0,0 +1,41 @@
+package dropDowns;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class oldStyle {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+            public void testOldStyle(){
+    driver.get("https://the-internet.herokuapp.com/dropdown");
+    WebElement dropdown= driver.findElement(By.id("dropdown"));
+
+    Select select =new Select(dropdown);
+
+    select.selectByValue("2");
+        System.out.println("by value::" + select.getFirstSelectedOption().getText());
+
+        select.selectByVisibleText("Option 1");
+        System.out.println("by visible text::" + select.getFirstSelectedOption().getText());
+
+    }
+    }
