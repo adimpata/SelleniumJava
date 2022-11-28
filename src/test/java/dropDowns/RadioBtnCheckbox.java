5ff75e845c6c542271a9d46fa274061e6568a7c1
Adrian <pata.adi@yahoo.com>
2022-11-29 00:01:04 +0200

    12:00

--- src/test/java/dropDowns/RadioBtnCheckbox.java
+++ src/test/java/dropDowns/RadioBtnCheckbox.java
@@ -1,4 +1,32 @@
 package dropDowns;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class RadioBtnCheckbox {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testRadioBtn() {
+        driver.get("https://semantic-ui.com/modules/checkbox.html");
+        WebElement radioBtn = driver.findElement(By.xpath("//label[contains(text(), 'Once a day') and preceding-sibling::input[@name='example2']]"));
+        radioBtn.click();
+    }
 }
